e0afbbd1390b5008435e36f9c4631ba5ede4cf50
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-02 03:57:54 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,29 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, integer)
+  integer.times do
+    puts message
+  end
+end
+
+def output_array(array)
+  for i in array
+    puts i
+  end
+end
+
+def return_string_array(array)
+  new_array = []
+  count = 0
+  for i in array
+    new_array[count] = i.to_s
+    count += 1
+  end
+  return new_array
+end
